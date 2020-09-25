cdc5c1a6a559c2dbb95ce933e84bc283ff87f569
vagorb <vagorb@ttu.ee>
2020-09-25 14:46:24 +0300

    back-end

---
+++ src/main/java/ee/taltech/iti02032020/backend/BackendApplication.java
@@ -0,0 +1,14 @@
+package ee.taltech.iti02032020.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/taltech/iti02032020/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti02032020.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
