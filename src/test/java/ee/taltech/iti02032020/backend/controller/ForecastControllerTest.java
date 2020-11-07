de88947f297bb2a930fac901b7d5194fba41ece8
vagorb <vagorb@ttu.ee>
2020-11-07 17:33:48 +0200

    gitlab ci file

--- src/test/java/ee/taltech/iti02032020/backend/controller/ForecastControllerTest.java
+++ src/test/java/ee/taltech/iti02032020/backend/controller/ForecastControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
