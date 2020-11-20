88bd1e59c6e350f0366339f47c021ef1c3062a07
vagorb <vagorb@ttu.ee>
2020-11-20 21:45:46 +0200

    theory part 2

--- src/main/java/ee/taltech/iti02032020/backend/b_theory/question11/FirstReadMeSOLID.java
+++ src/main/java/ee/taltech/iti02032020/backend/b_theory/question11/FirstReadMeSOLID.java
@@ -8,7 +8,7 @@ public class FirstReadMeSOLID {
     // SOLID is an acronym for 5 design principles intended to make software designs more understandable, flexible, and maintainable.
     // I would like you to tell me what solid is and then explain in detail each part of the principle.
     // What is solid?
-
+    // S (Nr1isS.java) stands for Single-responsibility and is done by Oleg Pahhomov as an example
     // O (Nr2isO.java)
     // L (Nr3isL.java)
     // I (Nr4isI.java)
