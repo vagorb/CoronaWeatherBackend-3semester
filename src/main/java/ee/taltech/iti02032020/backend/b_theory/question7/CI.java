8a5a6601cf21350876044132c0fe8fe6674389b2
Dima Voronoi <dvoron@ttu.ee>
2020-11-21 00:05:15 +0200

    Answers to question 7 and 8 of theory package

--- src/main/java/ee/taltech/iti02032020/backend/b_theory/question7/CI.java
+++ src/main/java/ee/taltech/iti02032020/backend/b_theory/question7/CI.java
@@ -4,28 +4,40 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Integration is a process of connecting your data, applications APIs, features and devices across your IT organization to be more
+    // efficient, productive and agile.
+
+
+
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer: In software engineering, continuous integration (CI) is the practice of merging all
+    // developers' working copies to a shared mainline several times a day
+    // var 2
+    // Continuous integration is a process of constant merging of branches and features to achieve a workflow where you have
+    // everybody working on same code and branch base, which also allows for the testers to test out the current set of combined
+    // features without being forced to test only specific parts of a feature or a branch
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
-    // [] test
+    // [x] compile
+    // [x] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 You can find bugs in the code the moment they were introduced with the latest push without having to manually search for it
+    // through a set of big changes that were recently merged in preparation for a release
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Developers have to spend a lot more time on creating tests because instead of developing a single feature in isolation
+    // they now also have to adjust it to changes/features developed by other developers in other branches constantly
+
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Travis CI
+    // 2 CircleCI
 }
--- src/main/java/ee/taltech/iti02032020/backend/b_theory/question8/CD.java
+++ src/main/java/ee/taltech/iti02032020/backend/b_theory/question8/CD.java
@@ -4,28 +4,37 @@ public class CD {
 
     //todo A
     // What is delivery?
-    // Answer:
+    // Answer: Preparation of compiled and tested code for a upcoming building/packaging process, the build/package will go through final
+    // testing phase before being submitted to be delivered to the user/client.
+
 
     //todo B
     // What makes delivery continuous?
-    // Answer:
+    // Answer: Continuous delivery is a process of constantly preparing builds/packages for possible deployment to clients/users
+    // in order to have working builds of a constantly developing project
+
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick which ones stand for continuous delivery
     // [] compile
     // [] test
-    // [] deploy
+    // [x] deploy
 
     //todo D
     // Name and explain 1 benefit of CD
-    // 1
+    // 1 No need to waste time on preparing the current build for a release, since the process has been automated and allows you to
+    // deploy the changes on regular basis, along with the fact that the difference between the last build and the current is minimal
+    // which is why it is a lot easier to fix potential bugs.
 
     //todo E
     // Name and explain 1 drawback of CD
-    // 1
+    // 1 Quality of a release/deployment depends on the strictness of the test cases, deployments that pass testing stage
+    // are not bug free and might impact the user on release even though they are not causing problems in the test cases.
+    // Also there is a strong need of a reliable integration server for the automated tests and secure product delivery
 
     //todo F
     // Which one is more important to start with CI or CD? Why?
-    // CI/CD:
-    // Because:
+    // CI/CD: CI
+    // Because: It ensures a more stable workflow the main goal of which is making the whole product being bug free,
+    // instead of working on fixing specific bugs of features that are yet to be integrated into the project
 }
