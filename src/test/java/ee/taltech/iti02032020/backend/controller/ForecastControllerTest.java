a7f8daef111f6a3e293820ad760a5266d8184921
vagorb <vagorb@ttu.ee>
2020-12-26 22:52:07 +0200

    Added users tests with mockmvc. Changed forecast tests to suit admin and user roles. Changed database configuration.

--- src/test/java/ee/taltech/iti02032020/backend/controller/ForecastControllerTest.java
+++ src/test/java/ee/taltech/iti02032020/backend/controller/ForecastControllerTest.java
@@ -1,38 +1,29 @@
 package ee.taltech.iti02032020.backend.controller;
 
+import ee.taltech.iti02032020.backend.controller.common.RestTemplate;
 import ee.taltech.iti02032020.backend.model.DailyForecast;
 import ee.taltech.iti02032020.backend.model.Forecast;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ForecastControllerTest {
 
+class ForecastControllerTest extends RestTemplate {
 
-    @Autowired
-    private TestRestTemplate testRestTemplate;
-
-    public static final ParameterizedTypeReference<List<String>> LIST_OF_FORECASTS = new ParameterizedTypeReference<>() {
-    };
-    public static final ParameterizedTypeReference<List<DailyForecast>> LIST_OF_FORECASTS_FOR_SEVEN_DAYS = new ParameterizedTypeReference<>() {
-    };
+    public static final String USER = "user";
+    public static final String ADMIN = "admin";
 
     @Test
     void deleteFromDatabase() {
         Forecast forecast1 = new Forecast("Estonia", "CityDoNotExistInDatabase1",null, null, "45", "45", null, null, null);
-        testRestTemplate.exchange("/Forecast", HttpMethod.POST, new HttpEntity<>(forecast1), Forecast.class);
-        testRestTemplate.exchange("/Forecast/2", HttpMethod.DELETE, null, Forecast.class);
+        testRestTemplate.exchange("/Forecast", HttpMethod.POST, entity(forecast1, ADMIN), Forecast.class);
+        testRestTemplate.exchange("/Forecast/2", HttpMethod.DELETE, entity(null, ADMIN), Forecast.class);
         ResponseEntity<List<String>> exchange = testRestTemplate.exchange("/Forecast",
                 HttpMethod.GET, null, LIST_OF_FORECASTS);
         List<String> cities = assertOk(exchange);
@@ -45,9 +36,9 @@ class ForecastControllerTest {
         Forecast forecast1 = new Forecast("Estonia", "CityDoNotExistInDatabase1",null, null, "45", "45", null, null, null);
         Forecast forecast2 = new Forecast("Estonia", "CityDoNotExistInDatabase2",null, null, "45", "45", null, null, null);
         Forecast forecast4 = new Forecast("Estonia", "CityDoNotExistInDatabase4",null, null, "45", "45", null, null, null);
-        testRestTemplate.exchange("/Forecast", HttpMethod.POST, new HttpEntity<>(forecast1), Forecast.class);
-        testRestTemplate.exchange("/Forecast", HttpMethod.POST, new HttpEntity<>(forecast2), Forecast.class);
-        testRestTemplate.exchange("/Forecast", HttpMethod.POST, new HttpEntity<>(forecast4), Forecast.class);
+        testRestTemplate.exchange("/Forecast", HttpMethod.POST, entity(forecast1, USER), Forecast.class);
+        testRestTemplate.exchange("/Forecast", HttpMethod.POST, entity(forecast2, USER), Forecast.class);
+        testRestTemplate.exchange("/Forecast", HttpMethod.POST, entity(forecast4, USER), Forecast.class);
         testRestTemplate.exchange("/Forecast/city/CityDoNotExistInDatabase4", HttpMethod.GET, null, Forecast.class);
         testRestTemplate.exchange("/Forecast/city/CityDoNotExistInDatabase4", HttpMethod.GET, null, Forecast.class);
         ResponseEntity<List<String>> exchange = testRestTemplate.exchange("/Forecast",
@@ -72,15 +63,15 @@ class ForecastControllerTest {
     @Test
     void getForecastFiveDays() {
         ResponseEntity<List<DailyForecast>> exchange = testRestTemplate.exchange("/Forecast/lat/50/lon/50",
-                HttpMethod.GET, null, LIST_OF_FORECASTS_FOR_SEVEN_DAYS);
+                HttpMethod.GET, entity(null, USER), LIST_OF_FORECASTS_FOR_SEVEN_DAYS);
         List<DailyForecast> forecasts = assertOk(exchange);
         assertEquals(8, forecasts.size());
     }
 
 
-    private <T> T assertOk(ResponseEntity<T> exchange) {
-        assertNotNull(exchange.getBody());
-        assertEquals(HttpStatus.OK, exchange.getStatusCode());
-        return exchange.getBody();
+
+    private <T> HttpEntity<T> entity(T msg, String username) {
+        HttpHeaders headers = authorizationHeader(username);
+        return new HttpEntity<>(msg, headers);
     }
 }
---
+++ src/test/java/ee/taltech/iti02032020/backend/controller/UserControllerTest.java
@@ -0,0 +1,90 @@
+package ee.taltech.iti02032020.backend.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import ee.taltech.iti02032020.backend.service.users.dto.LoginDto;
+
+import ee.taltech.iti02032020.backend.service.users.dto.RegisterDto;
+
+import ee.taltech.iti02032020.backend.service.users.dto.UpdateDto;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    @Order(1)
+    public void registerUser() throws Exception {
+        RegisterDto newUser = new RegisterDto();
+        newUser.setUsername("NotUser");
+        newUser.setHometown("Valga");
+        newUser.setPassword("Hello");
+        mvc.perform(MockMvcRequestBuilders.post("/users/register").contentType(APPLICATION_JSON)
+                .content(asJsonString(newUser)).accept(APPLICATION_JSON)).andExpect(status().isCreated());
+
+    }
+
+    @Test
+    @Order(2)
+    public void login() throws Exception {
+        LoginDto login = new LoginDto();
+        login.setUsername("user");
+        login.setPassword("user");
+        mvc.perform(MockMvcRequestBuilders.post("/users/login").contentType(APPLICATION_JSON)
+                .content(asJsonString(login)).accept(APPLICATION_JSON)).andExpect(status().isOk());
+
+    }
+
+    @Test
+    @Order(3)
+    @WithMockUser(username = "user", password = "user", roles = "USER")
+    public void updateUserNoTown() throws Exception {
+        UpdateDto updateUser = new UpdateDto();
+        updateUser.setUsername("Hello");
+        updateUser.setHometown("Hello");
+        updateUser.setOldusername("user");
+        mvc.perform(MockMvcRequestBuilders.post("/users/update").contentType(APPLICATION_JSON)
+                .content(asJsonString(updateUser)).accept(APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
+
+
+    static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
